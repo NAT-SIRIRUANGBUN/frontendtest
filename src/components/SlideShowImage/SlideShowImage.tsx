737ac7f2931c4af151bdf978fed529e2e28e5a62
NATCH <nat.siriruangbun@gmail.com>
2024-03-28 08:03:54 +0700

    Change Dic Name

--- src/components/SlideShowImage/SlideShowImage.tsx
+++ src/components/SlideShowImage/SlideShowImage.tsx
@@ -3,7 +3,7 @@ const ImageCount = 10
 const ImageList:string[] = []
 
 for (let i = 1 ; i <= ImageCount ; i++) {
-    ImageList.push('/SlideshowImage/' + i + '.jpeg')
+    ImageList.push('/SlideShowImage/' + i + '.jpeg')
 }
 
 export default ImageList 
