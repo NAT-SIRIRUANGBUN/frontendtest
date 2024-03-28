3120d4c41ade0ff96af73ec55d9c948d7122b85a
NATCH <nat.siriruangbun@gmail.com>
2024-03-28 16:25:59 +0700

    Change Font Directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,13 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions)
   return (
     <html lang="en">
-      <head> <title>CEDT JOB FAIR</title></head>
+      <head> 
+        <title>CEDT JOB FAIR</title>
+        
+        
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+      
+      </head>
       <body>
         <NextAuthProvider session={session}>
             <NavBar/>
