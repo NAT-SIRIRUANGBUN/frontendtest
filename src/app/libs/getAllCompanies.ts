42185289bccf055387b145cba2fe577bd1414e50
NATCH <nat.siriruangbun@gmail.com>
2024-03-28 15:51:23 +0700

    TE

--- src/app/libs/getAllCompanies.ts
+++ src/app/libs/getAllCompanies.ts
@@ -5,7 +5,7 @@ import { revalidateTag } from "next/cache"
 export default async function getAllCompanies () {
 
     const backend_url = process.env.BACKEND_URL
-    const allCompanies = await (await fetch('http://localhost:1234/api/companies' , {next : {tags : ['allCompanies']}})).json()
+    const allCompanies = await (await fetch(`${backend_url}/api/companies` , {next : {tags : ['allCompanies']}})).json()
 
     
     return allCompanies
